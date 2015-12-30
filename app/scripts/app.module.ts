940a974a5011f6d7887b88eb817bf74fc1f8e64b
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2015-12-30 19:33:46 +0100

    added angular2 and ngUpgrade

--- app/scripts/app.module.ts
+++ app/scripts/app.module.ts
@@ -2,9 +2,12 @@
 /// <reference path="../typings/angularjs/angular-resource.d.ts" />
 /// <reference path="../typings/angularjs/angular-route.d.ts" />
 
-'use strict';
+import {UpgradeAdapter} from 'angular2/upgrade';
+var adapter = new UpgradeAdapter();
 
 angular.module('emuwebApp', ['ui', 'ngAnimate', 'angular.filter', 'btford.markdown'])
 	.config(function ($locationProvider) {
 		$locationProvider.html5Mode(true);
-  });
+	});
+
+adapter.bootstrap(document.body, ['emuwebApp']);
